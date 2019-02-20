34208e6a03e89f07b6f68a2b7e3c1b65904c8cd0
Ben Hargreaves <bhargreaves91@gmail.com>
2019-02-20 00:32:24 -0500

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CitiesDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            //init dummy data
+            Cities = new List<CitiesDto>()
+            {
+                new CitiesDto()
+                {
+                    Id = 1,
+                    Name = "Toronto",
+                    Description = "Cold and then Construction."
+                },
+                new CitiesDto()
+                {
+                    Id = 2,
+                    Name = "New York City",
+                    Description = "Home of the Teenage Mutant Ninja Turtles"
+                },
+                new CitiesDto()
+                {
+                    Id = 3,
+                    Name = "Brisbane",
+                    Description = "Sunny times always."
+                }
+            };
+        }
+    }
+}
